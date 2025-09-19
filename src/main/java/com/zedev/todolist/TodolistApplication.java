fe4787ff01c1dc0632108f93afea901b0c37aa32
José Petrucio <jrazevedo.lps@gmail.com>
2025-09-19 17:37:20 -0300

    criar todolist

---
+++ src/main/java/com/zedev/todolist/TodolistApplication.java
@@ -0,0 +1,13 @@
+package com.zedev.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication; //executa a aplicação
+
+@SpringBootApplication //classe inicial do projeto
+public class TodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/zedev/todolist/user/UserModel.java
@@ -0,0 +1,13 @@
+package com.zedev.todolist.user;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+
+    private String userName;
+    private String name;
+    private String password;
+
+}
+
